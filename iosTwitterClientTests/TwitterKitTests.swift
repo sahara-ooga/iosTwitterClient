a2c703010683ca55c9f99951e1dd0d45cdec1e58
yuu ogasawara <y.ogasawara@st-ventures.jp>
2018-03-19 23:05:59 +0900

    Introduce TwitterKit via Carthage

---
+++ iosTwitterClientTests/TwitterKitTests.swift
@@ -0,0 +1,31 @@
+//
+//  TwitterKitTests.swift
+//  iosTwitterClientTests
+//
+//  Created by yogasawara@stv on 2018/03/19.
+//  Copyright © 2018年 sunday carpenter. All rights reserved.
+//
+
+import XCTest
+@testable import iosTwitterClient
+import TwitterKit
+import TwitterCore
+
+class TwitterKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+}
